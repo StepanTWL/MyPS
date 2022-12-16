8bb89d4d32f9ac4c79c288498bcda4d87f699ff5
StepanTWL <h3xp3ctb@gmail.com>
2022-12-16 21:40:17 +0200

    16.12.2022 homw

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 import tkinter as tk
 from tkinter import filedialog
 
+text_editor = tk.Text()
 
-def get_file_name():
-    file = filedialog.askopenfilename()
+
+def open_file():
+    filepath = tk.filedialog.askopenfilename()
+    if filepath != "":
+        with open(filepath, "r") as file:
+            image_file = tk.PhotoImage(file=file)
+
+
+def save_file():
+    filepath = filedialog.asksaveasfilename()
+    """
+    if filepath != "":
+        text = text_editor.get("1.0", END)
+        with open(filepath, "w") as file:
+            file.write(text)
+    """
+
 
 window = tk.Tk()
-icon = tk.PhotoImage(file='Image\icon.png')
-window.iconphoto(False, icon)
+# icon = tk.PhotoImage(file='Image\icon.png')
+# window.iconphoto(False, icon)
 window.config(bg='white')  # белый фон
 window.title('My Photoshop')
 window.geometry('500x600+800+100')
 window.resizable(False, False)  # блокировка изменения размера окна
 
-#btn_open = tk.Button(window, text='Open')
-btn_save = tk.Button(window, text='Save')
 btn_turn_left = tk.Button(window, text='Left')
 btn_turn_right = tk.Button(window, text='Right')
 btn_mirror_hor = tk.Button(window, text='Hor')
 btn_mirror_vert = tk.Button(window, text='Vert')
 
-btn_open = tk.Button(window, text='Open', command=get_file_name)
+btn_open = tk.Button(window, text='Open', command=open_file)
+btn_save = tk.Button(window, text='Save', command=save_file)
 
+"""
 our_image = tk.PhotoImage(file="Image\_no-image.png")
 our_lable = tk.Label(window)
 our_lable.image = our_image
 our_lable['image'] = our_lable.image
 our_lable.place(x=100, y=50)
+"""
 
 btn_open.place(x=370, y=550, width=100, height=30)
 btn_save.place(x=260, y=550, width=100, height=30)
