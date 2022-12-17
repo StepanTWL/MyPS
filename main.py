300683d1ba6e8061b4b9fb11de38d6d35415d7b2
StepanTWL <h3xp3ctb@gmail.com>
2022-12-17 21:08:11 +0200

    17.12.2022 home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from PIL import Image
 
 text_editor = tk.Text()
 
@@ -12,13 +12,62 @@ def open_file():
 
 
 def save_file():
-    filepath = filedialog.asksaveasfilename()
+    filepath = tk.filedialog.asksaveasfilename()
     """
     if filepath != "":
         text = text_editor.get("1.0", END)
         with open(filepath, "w") as file:
             file.write(text)
     """
+
+
+def create_horizontal_image(image_path):
+    image_pixels_list = []
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+    # Добавляем вертикальные линии в обратном порядке
+    for w in range(width):
+        vertical_line = []
+        for h in range(height):
+            vertical_line.append(pixels[w, h])
+        image_pixels_list.insert(0, vertical_line)
+
+    # Записываем готовые пиксели
+    for w in range(width):
+        for h in range(height):
+            pixels[w, h] = image_pixels_list[w][h]
+    # img.save(f"{int(time.time())}.jpeg")
+
+
+def create_vertical_image(image_path):
+    image_pixels_list = []
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+    # Добавляем вертикальные линии в обратном порядке
+    for h in range(height):#may be width-1
+        horizontal_line = []
+        for w in range(width):#may be height-1
+            horizontal_line.append(pixels[w, h])
+        image_pixels_list.insert(0, horizontal_line)
+
+    # Записываем готовые пиксели
+    for w in range(width):
+        for h in range(height):
+            pixels[w, h] = image_pixels_list[w][h]
+    # img.save(f"{int(time.time())}.jpeg")
+
+
+def create_left_image(image_path):
+    image_pixels_list = []
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+
 
 
 window = tk.Tk()
