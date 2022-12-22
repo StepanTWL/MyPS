ea30a8880eaadda69455daa5d18cea12a6109350
StepanTWL <h3xp3ctb@gmail.com>
2022-12-22 18:04:23 +0200

    22.12.2022 work

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from tkinter import filedialog as fd
 
 
-# text_editor = tk.Text()
+#text_editor = tk.Text()
 
 
 def open_file():
-    filepath = tk.filedialog.askopenfilename()
+    filetypes = (
+        ('JPG, JPEG', '*.jpg *.jpeg'),
+        ('BMP', '*.bmp'),
+        ('PNG', '*.png'),
+    )
+
+    filepath = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
     if filepath != "":
         with open(filepath, "r") as file:
             image_file = tk.PhotoImage(file=file)
 
 
 def save_file():
-    filepath = tk.filedialog.asksaveasfilename()
+    filepath = fd.asksaveasfilename()
     """
     if filepath != "":
         text = text_editor.get("1.0", END)
@@ -107,30 +118,39 @@ window.title('My Photoshop')
 window.geometry('500x600+800+100')
 window.resizable(False, False)  # блокировка изменения размера окна
 
-image = Image.open(r'Image\turn-left-button.png')
-image = image.resize((20,20), Image.ANTIALIAS)
-image = ImageTk.PhotoImage(image)
-btn_turn_left = tk.Button(window, image=image)
-# image = ImageTk.PhotoImage(file=r'Image\turn-right-button.png-button.png')
-# btn_turn_right = tk.Button(window, image=image)
-btn_mirror_hor = tk.Button(window, text='Hor')
-btn_mirror_vert = tk.Button(window, text='Vert')
+image_left = Image.open(r'Image\turn-left-button.png')
+image_left = image_left.resize((20, 20), Image.Resampling.LANCZOS)
+image_left = ImageTk.PhotoImage(image_left)
+btn_turn_left = tk.Button(window, image=image_left)
+image_right = Image.open(r'Image\turn-right-button.png')
+image_right = image_right.resize((20, 20), Image.Resampling.LANCZOS)
+image_right = ImageTk.PhotoImage(image_right)
+btn_turn_right = tk.Button(window, image=image_right)
+image_horiz = Image.open(r'Image\mirror-horizontal-button.png')
+image_horiz = image_horiz.resize((20, 20), Image.Resampling.LANCZOS)
+image_horiz = ImageTk.PhotoImage(image_horiz)
+btn_mirror_hor = tk.Button(window, image=image_horiz)
+image_vertic = Image.open(r'Image\mirror-vertical-button.png')
+image_vertic = image_vertic.resize((20, 20), Image.Resampling.LANCZOS)
+image_vertic = ImageTk.PhotoImage(image_vertic)
+btn_mirror_vert = tk.Button(window, image=image_vertic)
 
 btn_open = tk.Button(window, text='Open', command=open_file)
 btn_save = tk.Button(window, text='Save', command=save_file)
 
-"""
-our_image = tk.PhotoImage(file="Image\_no-image.png")
+
+
+our_image = tk.PhotoImage(file=r'Image\no-image.png')
 our_lable = tk.Label(window)
 our_lable.image = our_image
 our_lable['image'] = our_lable.image
 our_lable.place(x=100, y=50)
-"""
+
 
 btn_open.place(x=370, y=550, width=100, height=30)
 btn_save.place(x=260, y=550, width=100, height=30)
 btn_turn_left.place(x=30, y=550, width=30, height=30)
-# btn_turn_right.place(x=70, y=550, width=30, height=30)
+btn_turn_right.place(x=70, y=550, width=30, height=30)
 btn_mirror_hor.place(x=110, y=550, width=30, height=30)
 btn_mirror_vert.place(x=150, y=550, width=30, height=30)
 window.grid_columnconfigure(0, minsize=70)
