2adcafc691ca80a1acb5772d8ffe6659ef6b5632
StepanTWL <h3xp3ctb@gmail.com>
2022-12-27 18:04:01 +0200

    27.12.2022 work

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from pathlib import Path
 
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 from tkinter import filedialog as fd
 
 
@@ -36,76 +36,53 @@ def save_file():
     """
 
 
-def create_horizontal_image(image_path):
-    image_pixels_list = []
-    img = Image.open(image_path)
-    width, height = img.size
-    pixels = img.load()
-
-    # Добавляем вертикальные линии в обратном порядке
-    for w in range(width):
-        vertical_line = []
-        for h in range(height):
-            vertical_line.append(pixels[w, h])
-        image_pixels_list.insert(0, vertical_line)
-
-    # Записываем готовые пиксели
-    for w in range(width):
-        for h in range(height):
-            pixels[w, h] = image_pixels_list[w][h]
-    # img.save(f"{int(time.time())}.jpeg")
+def create_horizontal_image(image):
+    image = ImageOps.mirror(image)
+    our_lable.image = ImageTk.PhotoImage(image)
+    our_lable['image'] = our_lable.image
+    our_lable.place(x=100, y=50)
 
 
-def create_vertical_image():
-    image_pixels_list = []
-    width, height = our_image.width, our_image.height
-    pixels = our_image.load()
-
-    # Добавляем вертикальные линии в обратном порядке
-    for h in range(height):  # may be width-1
-        horizontal_line = []
-        for w in range(width):  # may be height-1
-            horizontal_line.append(pixels[w, h])
-        image_pixels_list.insert(0, horizontal_line)
-
-    # Записываем готовые пиксели
-    for w in range(width):
-        for h in range(height):
-            pixels[w, h] = image_pixels_list[w][h]
-
-    # our_image = Image.new(pixels)
-    # our_image.save(pixels)
-    # img.save(f"{int(time.time())}.jpeg")
+def create_vertical_image(image):
+    image = ImageOps.flip(image)
+    our_lable.image = ImageTk.PhotoImage(image)
+    our_lable['image'] = our_lable.image
+    our_lable.place(x=100, y=50)
 
 
-class rotate_left_image:
+class create_image:
 
     def __init__(self):
-        self.count = 1
+        self.count = 0
 
-    def func(self, image):
-        image = image.rotate(angle=90 * self.count, expand=True)
+    def left(self, image):
+        self.count += 1
+        image = image.rotate(angle=90 * self.count, expand=True).copy()
         our_lable.image = ImageTk.PhotoImage(image)
         our_lable['image'] = our_lable.image
         our_lable.place(x=100, y=50)
-        self.count += 1
 
-rotate_left_image = rotate_left_image()
-
-class rotate_right_image:
-
-    def __init__(self):
-        self.count = 1
-
-    def func(self, image):
-        image = image.rotate(angle=270 * self.count, expand=True)
+    def right(self, image):
+        self.count -= 1
+        image = image.rotate(angle=90 * self.count, expand=True).copy()
         our_lable.image = ImageTk.PhotoImage(image)
         our_lable['image'] = our_lable.image
         our_lable.place(x=100, y=50)
-        self.count += 1
 
-rotate_right_image = rotate_right_image()
+    def horizontal(self, image):
+        image = ImageOps.mirror(image).copy()
+        our_lable.image = ImageTk.PhotoImage(image)
+        our_lable['image'] = our_lable.image
+        our_lable.place(x=100, y=50)
 
+    def vertical(self, image):
+        image = ImageOps.flip(image).copy()
+        our_lable.image = ImageTk.PhotoImage(image)
+        our_lable['image'] = our_lable.image
+        our_lable.place(x=100, y=50)
+
+
+create_image = create_image()
 
 window = tk.Tk()
 window.iconphoto(False, ImageTk.PhotoImage(file=Path('Image', 'icon.png')))
@@ -114,35 +91,31 @@ window.title('My Photoshop')
 window.geometry('500x600+800+100')
 window.resizable(False, False)  # блокировка изменения размера окна
 
-
 our_image = Image.open(Path('Image', 'no-image.png'))
 our_lable = tk.Label(window)
 our_lable.image = ImageTk.PhotoImage(our_image)
 our_lable['image'] = our_lable.image
 our_lable.place(x=100, y=50)
 
-
 image_left = Image.open(Path('Image', 'turn-left-button.png'))
 image_left = image_left.resize((20, 20), Image.Resampling.LANCZOS)
 image_left = ImageTk.PhotoImage(image_left)
-btn_turn_left = tk.Button(window, image=image_left, command= lambda: rotate_left_image.func(our_image))
+btn_turn_left = tk.Button(window, image=image_left, command=lambda: create_image.left(our_image))
 image_right = Image.open(Path('Image', 'turn-right-button.png'))
 image_right = image_right.resize((20, 20), Image.Resampling.LANCZOS)
 image_right = ImageTk.PhotoImage(image_right)
-btn_turn_right = tk.Button(window, image=image_right, command= lambda: rotate_right_image.func(our_image))
+btn_turn_right = tk.Button(window, image=image_right, command=lambda: create_image.right(our_image))
 image_horiz = Image.open(Path('Image', 'mirror-horizontal-button.png'))
 image_horiz = image_horiz.resize((20, 20), Image.Resampling.LANCZOS)
 image_horiz = ImageTk.PhotoImage(image_horiz)
-btn_mirror_hor = tk.Button(window, image=image_horiz)
+btn_mirror_hor = tk.Button(window, image=image_horiz, command=lambda: create_image.horizontal(our_image))
 image_vertic = Image.open(Path('Image', 'mirror-vertical-button.png'))
 image_vertic = image_vertic.resize((20, 20), Image.Resampling.LANCZOS)
 image_vertic = ImageTk.PhotoImage(image_vertic)
-btn_mirror_vert = tk.Button(window, image=image_vertic, command=create_vertical_image)
-
+btn_mirror_vert = tk.Button(window, image=image_vertic, command=lambda: create_image.vertical(our_image))
 
 btn_open = tk.Button(window, text='Open', command=open_file)
 btn_save = tk.Button(window, text='Save', command=save_file)
-
 
 btn_open.place(x=370, y=550, width=100, height=30)
 btn_save.place(x=260, y=550, width=100, height=30)
@@ -151,6 +124,5 @@ btn_turn_right.place(x=70, y=550, width=30, height=30)
 btn_mirror_hor.place(x=110, y=550, width=30, height=30)
 btn_mirror_vert.place(x=150, y=550, width=30, height=30)
 window.grid_columnconfigure(0, minsize=70)
-
 
 window.mainloop()
